7c8a2600337df50a9bd43965248d4ab89888a91b
Julian Obst <obstjn@studi.informatik.uni-stuttgart.de>
2022-08-01 14:59:19 +0200

    landscape approximation with subgraph optimization.

--- landscape_approximation-v2.py
+++ landscape_approximation-v2.py
@@ -10,6 +10,8 @@ from graph_management import *
 
 from qiskit.visualization import plot_histogram
 from qiskit import Aer, execute
+from scipy.optimize import minimize
+from time import time
 
 
 ws_numbers_4reg0 = [0, 1, 3, 7, 24, 25, 27, 33, 35, 57, 23, 19, 17, 8, 15, 11, 9, 51, 41, 43] 
@@ -51,3 +53,68 @@ for typ, ws, occ in sg_data:
 #plt.savefig('landscape-apx_true-energy.pdf')
 #plt.show()
 
+""" ------------- subgraph optimization ------------- """
+
+def objective_function_apx(params):
+  gamma, beta = params
+  subgraph = nx.read_adjlist('./graphs/4reg0.graph', nodetype=int)
+  edge  = (3,4)
+
+  qc0 = qaoa_circuit(subgraph, number_to_ws(19, len(subgraph)), eps=.1)
+  qc1 = qaoa_circuit(subgraph, number_to_ws(23, len(subgraph)), eps=.1)
+
+  energy0 = get_energy(subgraph, qc0, gamma, beta, edge, shots=1)
+  energy1 = get_energy(subgraph, qc1, gamma, beta, edge, shots=1)
+
+  return -(11 * energy0 + 9 * energy1)
+
+
+def objective_function_true(params, *args):
+  G, apx_sol, subgraph_data = args
+  energies = []
+  gamma, beta = params
+
+  for item in subgraph_data:
+    subgraph, edge, occurrence = item
+    apxsol = np.array([w for node, w in nx.get_node_attributes(subgraph, 'weight').items()])
+    qc = qaoa_circuit(subgraph, apxsol, eps=.1)
+
+    energies.append(occurrence * get_energy(subgraph, qc, gamma, beta, edge, shots=1))
+
+  return -sum(energies)
+
+"""
+print('\noptimization apx:')
+start = time()
+np.random.seed(42)
+gam, bet = maximizing_parameters(apx_energy, plotting=False)
+gam, bet = gam[0], bet[0]
+xinit = np.array([gam, bet])
+
+optimized_result = minimize(objective_function_apx, x0=xinit, method='COBYLA')
+gam_apx = optimized_result.x[0]
+bet_apx = optimized_result.x[1]
+print()
+print(optimized_result)
+end = time()
+print(f'time elapsed: {end-start} s')
+
+print('\noptimization true:')
+start = time()
+np.random.seed(42)
+gam, bet = maximizing_parameters(true_energy, plotting=False)
+gam, bet = gam[0], bet[0]
+xinit = np.array([gam, bet])
+optimized_result = minimize(objective_function_true, x0=xinit, method='COBYLA', args=(G, apx, get_ws_subgraphs(G, apx)))
+gam_true = optimized_result.x[0]
+bet_true = optimized_result.x[1]
+print()
+print(optimized_result)
+end = time()
+print(f'time elapsed: {end-start} s')
+
+maxcut_val = cut_value(G, maxcut(G))
+apx_params_value = objective_function_true((gam_apx, bet_apx), G, apx, get_ws_subgraphs(G, apx))
+print(f'apx ratio of apx params: {-apx_params_value/maxcut_val}')
+print(f'apx ratio of opt params: {-optimized_result.fun/maxcut_val}')
+"""
